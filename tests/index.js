8e8f8c396d5c769a6fcf2533c9396ecb237d1c9c
Benjy Cui <benjytrys@gmail.com>
2017-06-29 10:12:09 +0800

    test: simplify test code

--- tests/index.js
+++ tests/index.js
@@ -4,72 +4,64 @@ const expect = require('expect.js');
 const Line = require('../').Line;
 const Circle = require('../').Circle;
 
-describe('line progress', () => {
-  it('works', () => {
-    const div = document.createElement(div);
+describe('Progress', () => {
+  let div = null;
+  beforeEach(() => {
+    div = document.createElement('div');
     document.body.appendChild(div);
-    const line = ReactDOM.render(<Line percent="30" strokeWidth="1" />, div);
-    expect(line.props.percent).to.be('30');
-    ReactDOM.unmountComponentAtNode(div);
-  });
-});
-
-describe('circle progress', () => {
-  it('works', () => {
-    const div = document.createElement(div);
-    document.body.appendChild(div);
-    const circle = ReactDOM.render(<Circle percent="30" strokeWidth="1" />, div);
-    expect(circle.props.percent).to.be('30');
-    ReactDOM.unmountComponentAtNode(div);
   });
 
-  it('gap degree bottom', () => {
-    const div = document.createElement(div);
-    document.body.appendChild(div);
-    const circle = ReactDOM.render(
-      <Circle percent="30" strokeWidth="1" gapDegree={70} gapPosition="bottom" />
-    , div);
-    expect(circle.props.percent).to.be('30');
+  afterEach(() => {
     ReactDOM.unmountComponentAtNode(div);
+    document.body.removeChild(div);
   });
 
-  it('gap degree top', () => {
-    const div = document.createElement(div);
-    document.body.appendChild(div);
-    const circle = ReactDOM.render(
-      <Circle percent="30" strokeWidth="1" gapDegree={70} gapPosition="top" />
-    , div);
-    expect(circle.props.percent).to.be('30');
-    ReactDOM.unmountComponentAtNode(div);
+  describe('Line', () => {
+    it('works', () => {
+      const line = ReactDOM.render(<Line percent="30" strokeWidth="1" />, div);
+      expect(line.props.percent).to.be('30');
+    });
   });
 
-  it('gap degree left', () => {
-    const div = document.createElement(div);
-    document.body.appendChild(div);
-    const circle = ReactDOM.render(
-      <Circle percent="30" strokeWidth="1" gapDegree={70} gapPosition="left" />
-    , div);
-    expect(circle.props.percent).to.be('30');
-    ReactDOM.unmountComponentAtNode(div);
-  });
+  describe('Circle', () => {
+    it('works', () => {
+      const circle = ReactDOM.render(<Circle percent="30" strokeWidth="1" />, div);
+      expect(circle.props.percent).to.be('30');
+    });
 
-  it('gap degree right', () => {
-    const div = document.createElement(div);
-    document.body.appendChild(div);
-    const circle = ReactDOM.render(
-      <Circle percent="30" strokeWidth="1" gapDegree={70} gapPosition="right" />
-    , div);
-    expect(circle.props.percent).to.be('30');
-    ReactDOM.unmountComponentAtNode(div);
-  });
+    it('gap degree bottom', () => {
+      const circle = ReactDOM.render(
+          <Circle percent="30" strokeWidth="1" gapDegree={70} gapPosition="bottom" />
+          , div);
+      expect(circle.props.percent).to.be('30');
+    });
 
-  it('doesn\'t render the circle path if at 0 percent', () => {
-    const div = document.createElement(div);
-    document.body.appendChild(div);
-    const circle = ReactDOM.render(<Circle percent="0" strokeWidth="1" />, div);
-    const circlePathsLength = document.getElementsByClassName('rc-progress-circle-path').length;
-    expect(circlePathsLength).to.be(0);
-    circle.forceUpdate(); // See: https://github.com/react-component/progress/issues/35
-    ReactDOM.unmountComponentAtNode(div);
+    it('gap degree top', () => {
+      const circle = ReactDOM.render(
+          <Circle percent="30" strokeWidth="1" gapDegree={70} gapPosition="top" />
+          , div);
+      expect(circle.props.percent).to.be('30');
+    });
+
+    it('gap degree left', () => {
+      const circle = ReactDOM.render(
+          <Circle percent="30" strokeWidth="1" gapDegree={70} gapPosition="left" />
+          , div);
+      expect(circle.props.percent).to.be('30');
+    });
+
+    it('gap degree right', () => {
+      const circle = ReactDOM.render(
+          <Circle percent="30" strokeWidth="1" gapDegree={70} gapPosition="right" />
+          , div);
+      expect(circle.props.percent).to.be('30');
+    });
+
+    it('doesn\'t render the circle path if at 0 percent', () => {
+      const circle = ReactDOM.render(<Circle percent="0" strokeWidth="1" />, div);
+      const circlePathsLength = document.getElementsByClassName('rc-progress-circle-path').length;
+      expect(circlePathsLength).to.be(0);
+      circle.forceUpdate(); // See: https://github.com/react-component/progress/issues/35
+    });
   });
 });
