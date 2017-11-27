ee2213e54e9cddbee44084526ebca3fe5fb7873a
afc163 <afc163@gmail.com>
2017-11-27 23:34:47 +0800

    Updates

--- examples/fast-progress.js
+++ examples/fast-progress.js
@@ -1,100 +1,98 @@
-webpackJsonp([0],[
-/* 0 */
-/***/ function(module, exports, __webpack_require__) {
+webpackJsonp([2],{
 
-	module.exports = __webpack_require__(1);
+/***/ 67:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(68);
 
 
-/***/ },
-/* 1 */
-/***/ function(module, exports, __webpack_require__) {
+/***/ }),
 
-	'use strict';
-	
-	var _classCallCheck2 = __webpack_require__(2);
-	
-	var _classCallCheck3 = _interopRequireDefault(_classCallCheck2);
-	
-	var _possibleConstructorReturn2 = __webpack_require__(3);
-	
-	var _possibleConstructorReturn3 = _interopRequireDefault(_possibleConstructorReturn2);
-	
-	var _inherits2 = __webpack_require__(72);
-	
-	var _inherits3 = _interopRequireDefault(_inherits2);
-	
-	__webpack_require__(80);
-	
-	var _react = __webpack_require__(81);
-	
-	var _react2 = _interopRequireDefault(_react);
-	
-	var _reactDom = __webpack_require__(116);
-	
-	var _reactDom2 = _interopRequireDefault(_reactDom);
-	
-	var _rcProgress = __webpack_require__(262);
-	
-	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-	
-	var App = function (_Component) {
-	  (0, _inherits3.default)(App, _Component);
-	
-	  function App() {
-	    (0, _classCallCheck3.default)(this, App);
-	
-	    var _this = (0, _possibleConstructorReturn3.default)(this, _Component.call(this));
-	
-	    _this.state = {
-	      percent: 0
-	    };
-	    _this.increase = _this.increase.bind(_this);
-	    _this.restart = _this.restart.bind(_this);
-	    return _this;
-	  }
-	
-	  App.prototype.componentDidMount = function componentDidMount() {
-	    this.increase();
-	  };
-	
-	  App.prototype.increase = function increase() {
-	    var percent = this.state.percent + 1;
-	    if (percent >= 100) {
-	      clearTimeout(this.tm);
-	      return;
-	    }
-	    this.setState({ percent: percent });
-	    this.tm = setTimeout(this.increase, 10);
-	  };
-	
-	  App.prototype.restart = function restart() {
-	    var _this2 = this;
-	
-	    clearTimeout(this.tm);
-	    this.setState({ percent: 0 }, function () {
-	      _this2.increase();
-	    });
-	  };
-	
-	  App.prototype.render = function render() {
-	    return _react2.default.createElement(
-	      'div',
-	      { style: { margin: 10, width: 200 } },
-	      _react2.default.createElement(_rcProgress.Circle, { strokeWidth: '6', percent: this.state.percent }),
-	      _react2.default.createElement(_rcProgress.Line, { strokeWidth: '4', percent: this.state.percent }),
-	      _react2.default.createElement(
-	        'button',
-	        { onClick: this.restart },
-	        'Restart'
-	      )
-	    );
-	  };
-	
-	  return App;
-	}(_react.Component);
-	
-	_reactDom2.default.render(_react2.default.createElement(App, null), document.getElementById('__react-content'));
+/***/ 68:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-/***/ }
-]);
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_babel_runtime_helpers_classCallCheck__ = __webpack_require__(5);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_babel_runtime_helpers_classCallCheck___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_babel_runtime_helpers_classCallCheck__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_babel_runtime_helpers_possibleConstructorReturn__ = __webpack_require__(6);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_babel_runtime_helpers_possibleConstructorReturn___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_babel_runtime_helpers_possibleConstructorReturn__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_babel_runtime_helpers_inherits__ = __webpack_require__(13);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_babel_runtime_helpers_inherits___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2_babel_runtime_helpers_inherits__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_rc_progress_assets_index_less__ = __webpack_require__(39);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_rc_progress_assets_index_less___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_3_rc_progress_assets_index_less__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_react__ = __webpack_require__(4);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_4_react__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5_react_dom__ = __webpack_require__(42);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5_react_dom___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_5_react_dom__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6_rc_progress__ = __webpack_require__(43);
+
+
+
+
+
+
+
+
+var App = function (_Component) {
+  __WEBPACK_IMPORTED_MODULE_2_babel_runtime_helpers_inherits___default()(App, _Component);
+
+  function App() {
+    __WEBPACK_IMPORTED_MODULE_0_babel_runtime_helpers_classCallCheck___default()(this, App);
+
+    var _this = __WEBPACK_IMPORTED_MODULE_1_babel_runtime_helpers_possibleConstructorReturn___default()(this, _Component.call(this));
+
+    _this.state = {
+      percent: 0
+    };
+    _this.increase = _this.increase.bind(_this);
+    _this.restart = _this.restart.bind(_this);
+    return _this;
+  }
+
+  App.prototype.componentDidMount = function componentDidMount() {
+    this.increase();
+  };
+
+  App.prototype.increase = function increase() {
+    var percent = this.state.percent + 1;
+    if (percent >= 100) {
+      clearTimeout(this.tm);
+      return;
+    }
+    this.setState({ percent: percent });
+    this.tm = setTimeout(this.increase, 10);
+  };
+
+  App.prototype.restart = function restart() {
+    var _this2 = this;
+
+    clearTimeout(this.tm);
+    this.setState({ percent: 0 }, function () {
+      _this2.increase();
+    });
+  };
+
+  App.prototype.render = function render() {
+    return __WEBPACK_IMPORTED_MODULE_4_react___default.a.createElement(
+      'div',
+      { style: { margin: 10, width: 200 } },
+      __WEBPACK_IMPORTED_MODULE_4_react___default.a.createElement(__WEBPACK_IMPORTED_MODULE_6_rc_progress__["a" /* Circle */], { strokeWidth: '6', percent: this.state.percent }),
+      __WEBPACK_IMPORTED_MODULE_4_react___default.a.createElement(__WEBPACK_IMPORTED_MODULE_6_rc_progress__["b" /* Line */], { strokeWidth: '4', percent: this.state.percent }),
+      __WEBPACK_IMPORTED_MODULE_4_react___default.a.createElement(
+        'button',
+        { onClick: this.restart },
+        'Restart'
+      )
+    );
+  };
+
+  return App;
+}(__WEBPACK_IMPORTED_MODULE_4_react__["Component"]);
+
+__WEBPACK_IMPORTED_MODULE_5_react_dom___default.a.render(__WEBPACK_IMPORTED_MODULE_4_react___default.a.createElement(App, null), document.getElementById('__react-content'));
+
+/***/ })
+
+},[67]);
 //# sourceMappingURL=fast-progress.js.map
--- examples/gap.js
+++ examples/gap.js
@@ -1,143 +1,138 @@
 webpackJsonp([1],{
 
-/***/ 0:
-/***/ function(module, exports, __webpack_require__) {
+/***/ 121:
+/***/ (function(module, exports, __webpack_require__) {
 
-	module.exports = __webpack_require__(276);
+module.exports = __webpack_require__(122);
 
 
-/***/ },
+/***/ }),
 
-/***/ 276:
-/***/ function(module, exports, __webpack_require__) {
+/***/ 122:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-	'use strict';
-	
-	var _classCallCheck2 = __webpack_require__(2);
-	
-	var _classCallCheck3 = _interopRequireDefault(_classCallCheck2);
-	
-	var _possibleConstructorReturn2 = __webpack_require__(3);
-	
-	var _possibleConstructorReturn3 = _interopRequireDefault(_possibleConstructorReturn2);
-	
-	var _inherits2 = __webpack_require__(72);
-	
-	var _inherits3 = _interopRequireDefault(_inherits2);
-	
-	__webpack_require__(80);
-	
-	var _react = __webpack_require__(81);
-	
-	var _react2 = _interopRequireDefault(_react);
-	
-	var _reactDom = __webpack_require__(116);
-	
-	var _reactDom2 = _interopRequireDefault(_reactDom);
-	
-	var _rcProgress = __webpack_require__(262);
-	
-	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-	
-	var Example = function (_Component) {
-	  (0, _inherits3.default)(Example, _Component);
-	
-	  function Example() {
-	    (0, _classCallCheck3.default)(this, Example);
-	
-	    var _this = (0, _possibleConstructorReturn3.default)(this, _Component.call(this));
-	
-	    _this.state = {
-	      percent: 30,
-	      color: '#3FC7FA'
-	    };
-	    _this.changeState = _this.changeState.bind(_this);
-	    return _this;
-	  }
-	
-	  Example.prototype.changeState = function changeState() {
-	    var colorMap = ['#3FC7FA', '#85D262', '#FE8C6A'];
-	    var value = parseInt(Math.random() * 100, 10);
-	    this.setState({
-	      percent: value,
-	      color: colorMap[parseInt(Math.random() * 3, 10)]
-	    });
-	  };
-	
-	  Example.prototype.render = function render() {
-	    var circleContainerStyle = {
-	      width: '200px',
-	      height: '200px'
-	    };
-	    return _react2.default.createElement(
-	      'div',
-	      null,
-	      _react2.default.createElement(
-	        'div',
-	        { style: circleContainerStyle },
-	        _react2.default.createElement(_rcProgress.Circle, {
-	          percent: this.state.percent,
-	          gapDegree: 70,
-	          gapPosition: 'top',
-	          strokeWidth: '6',
-	          strokeLinecap: 'square',
-	          strokeColor: this.state.color
-	        })
-	      ),
-	      _react2.default.createElement(
-	        'div',
-	        { style: circleContainerStyle },
-	        _react2.default.createElement(_rcProgress.Circle, {
-	          percent: this.state.percent,
-	          gapDegree: 70,
-	          gapPosition: 'bottom',
-	          strokeWidth: '6',
-	          strokeLinecap: 'square',
-	          strokeColor: this.state.color
-	        })
-	      ),
-	      _react2.default.createElement(
-	        'div',
-	        { style: circleContainerStyle },
-	        _react2.default.createElement(_rcProgress.Circle, {
-	          percent: this.state.percent,
-	          gapDegree: 70,
-	          gapPosition: 'left',
-	          strokeWidth: '6',
-	          strokeLinecap: 'square',
-	          strokeColor: this.state.color
-	        })
-	      ),
-	      _react2.default.createElement(
-	        'div',
-	        { style: circleContainerStyle },
-	        _react2.default.createElement(_rcProgress.Circle, {
-	          percent: this.state.percent,
-	          gapDegree: 70,
-	          gapPosition: 'right',
-	          strokeWidth: '6',
-	          strokeLinecap: 'square',
-	          strokeColor: this.state.color
-	        })
-	      ),
-	      _react2.default.createElement(
-	        'p',
-	        null,
-	        _react2.default.createElement(
-	          'button',
-	          { onClick: this.changeState },
-	          'Change State'
-	        )
-	      )
-	    );
-	  };
-	
-	  return Example;
-	}(_react.Component);
-	
-	_reactDom2.default.render(_react2.default.createElement(Example, null), document.getElementById('__react-content'));
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_babel_runtime_helpers_classCallCheck__ = __webpack_require__(5);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_babel_runtime_helpers_classCallCheck___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_babel_runtime_helpers_classCallCheck__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_babel_runtime_helpers_possibleConstructorReturn__ = __webpack_require__(6);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_babel_runtime_helpers_possibleConstructorReturn___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_babel_runtime_helpers_possibleConstructorReturn__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_babel_runtime_helpers_inherits__ = __webpack_require__(13);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_babel_runtime_helpers_inherits___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2_babel_runtime_helpers_inherits__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_rc_progress_assets_index_less__ = __webpack_require__(39);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_rc_progress_assets_index_less___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_3_rc_progress_assets_index_less__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_react__ = __webpack_require__(4);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_4_react__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5_react_dom__ = __webpack_require__(42);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5_react_dom___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_5_react_dom__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6_rc_progress__ = __webpack_require__(43);
 
-/***/ }
 
-});
+
+
+
+
+
+
+var Example = function (_Component) {
+  __WEBPACK_IMPORTED_MODULE_2_babel_runtime_helpers_inherits___default()(Example, _Component);
+
+  function Example() {
+    __WEBPACK_IMPORTED_MODULE_0_babel_runtime_helpers_classCallCheck___default()(this, Example);
+
+    var _this = __WEBPACK_IMPORTED_MODULE_1_babel_runtime_helpers_possibleConstructorReturn___default()(this, _Component.call(this));
+
+    _this.state = {
+      percent: 30,
+      color: '#3FC7FA'
+    };
+    _this.changeState = _this.changeState.bind(_this);
+    return _this;
+  }
+
+  Example.prototype.changeState = function changeState() {
+    var colorMap = ['#3FC7FA', '#85D262', '#FE8C6A'];
+    var value = parseInt(Math.random() * 100, 10);
+    this.setState({
+      percent: value,
+      color: colorMap[parseInt(Math.random() * 3, 10)]
+    });
+  };
+
+  Example.prototype.render = function render() {
+    var circleContainerStyle = {
+      width: '200px',
+      height: '200px'
+    };
+    return __WEBPACK_IMPORTED_MODULE_4_react___default.a.createElement(
+      'div',
+      null,
+      __WEBPACK_IMPORTED_MODULE_4_react___default.a.createElement(
+        'div',
+        { style: circleContainerStyle },
+        __WEBPACK_IMPORTED_MODULE_4_react___default.a.createElement(__WEBPACK_IMPORTED_MODULE_6_rc_progress__["a" /* Circle */], {
+          percent: this.state.percent,
+          gapDegree: 70,
+          gapPosition: 'top',
+          strokeWidth: '6',
+          strokeLinecap: 'square',
+          strokeColor: this.state.color
+        })
+      ),
+      __WEBPACK_IMPORTED_MODULE_4_react___default.a.createElement(
+        'div',
+        { style: circleContainerStyle },
+        __WEBPACK_IMPORTED_MODULE_4_react___default.a.createElement(__WEBPACK_IMPORTED_MODULE_6_rc_progress__["a" /* Circle */], {
+          percent: this.state.percent,
+          gapDegree: 70,
+          gapPosition: 'bottom',
+          strokeWidth: '6',
+          strokeLinecap: 'square',
+          strokeColor: this.state.color
+        })
+      ),
+      __WEBPACK_IMPORTED_MODULE_4_react___default.a.createElement(
+        'div',
+        { style: circleContainerStyle },
+        __WEBPACK_IMPORTED_MODULE_4_react___default.a.createElement(__WEBPACK_IMPORTED_MODULE_6_rc_progress__["a" /* Circle */], {
+          percent: this.state.percent,
+          gapDegree: 70,
+          gapPosition: 'left',
+          strokeWidth: '6',
+          strokeLinecap: 'square',
+          strokeColor: this.state.color
+        })
+      ),
+      __WEBPACK_IMPORTED_MODULE_4_react___default.a.createElement(
+        'div',
+        { style: circleContainerStyle },
+        __WEBPACK_IMPORTED_MODULE_4_react___default.a.createElement(__WEBPACK_IMPORTED_MODULE_6_rc_progress__["a" /* Circle */], {
+          percent: this.state.percent,
+          gapDegree: 70,
+          gapPosition: 'right',
+          strokeWidth: '6',
+          strokeLinecap: 'square',
+          strokeColor: this.state.color
+        })
+      ),
+      __WEBPACK_IMPORTED_MODULE_4_react___default.a.createElement(
+        'p',
+        null,
+        __WEBPACK_IMPORTED_MODULE_4_react___default.a.createElement(
+          'button',
+          { onClick: this.changeState },
+          'Change State'
+        )
+      )
+    );
+  };
+
+  return Example;
+}(__WEBPACK_IMPORTED_MODULE_4_react__["Component"]);
+
+__WEBPACK_IMPORTED_MODULE_5_react_dom___default.a.render(__WEBPACK_IMPORTED_MODULE_4_react___default.a.createElement(Example, null), document.getElementById('__react-content'));
+
+/***/ })
+
+},[121]);
 //# sourceMappingURL=gap.js.map
